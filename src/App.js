f468e82f2e0a73e8dad61b22d99a2c515f8596a4
Tomasz Malitowicz <tomasz.malitowicz@gmail.com>
2020-03-20 10:38:15 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, NavLink, Switch } from 'react-router-dom';
+
+const Home = () => {
+  console.log('renderuje')
+  return (
+    <h1>Strona Startowa</h1>
+  );
+}
+const News = () => <h1>Aktualności</h1>;
+const Contact = () => <h1>Kontakt do nas</h1>;
+const ErrorPage = () => <h1>Strona nie istnieje</h1>
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <header>
+            <nav><ul>
+              <li><NavLink to="/" exact activeClassName="home_selected">Start</NavLink></li>
+              <li><NavLink to="/news" activeClassName="news_selected">Aktualności</NavLink></li>
+              <li><NavLink to="/contact" activeClassName="contact_selected">Kontakt</NavLink></li>
+            </ul></nav>
+          </header>
+          <section>
+            <Switch>
+              <Route path="/" exact component={Home} />
+              <Route path="/news" component={News} />
+              <Route path="/contact" component={Contact} />
+              <Route component={ErrorPage} />
+            </Switch>
+          </section>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
